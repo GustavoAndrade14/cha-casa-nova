b69cf12fa64546c1b769314ba406f7ee2a52b701
GustavoAndrade14 <GustaAndrade14@gmail.com>
2026-05-12 16:49:01 -0300

    removido o terceiro paço de Como Usar o Site

--- app/page.tsx
+++ app/page.tsx
@@ -106,7 +106,7 @@ export default function Home() {
             <h2 className="text-3xl font-bold text-yellow-400 mb-6 text-center">
               Como Usar o Site
             </h2>
-            <div className="grid md:grid-cols-3 gap-6">
+            <div className="grid md:grid-cols-2 gap-6">
               <div className="text-center">
                 <div className="w-12 h-12 rounded-full bg-yellow-400 text-black font-bold text-xl flex items-center justify-center mx-auto mb-4">
                   1
@@ -123,14 +123,14 @@ export default function Home() {
                   Clique no botão Comprar para ser redirecionado à loja
                 </p>
               </div>
-              <div className="text-center">
+              {/* <div className="text-center">
                 <div className="w-12 h-12 rounded-full bg-yellow-400 text-black font-bold text-xl flex items-center justify-center mx-auto mb-4">
                   3
                 </div>
                 <p className="text-gray-300">
                   Escolha o presente e nos presenteie com sua contribuição
                 </p>
-              </div>
+              </div> */}
             </div>
           </CardContent>
         </Card>
